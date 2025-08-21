22b8444100d40df040ded781c36b2664e6d262e4
Gourav2205 <gouravbiswas897@gmail.com>
2025-08-21 13:51:13 +0530

    04

---
+++ src/app/(app)/(home)/page.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">
+            I am a Button
+          </Button>
+        </div>
+        <div>
+          <Input
+            placeholder="I am a Input"
+          />
+        </div>
+        <div>
+          <Progress
+            value={50}
+          />
+        </div>
+        <div>
+          <Checkbox
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
